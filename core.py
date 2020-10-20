7c8880e44b65dc7734fecdc1f7a5ff8edc29fb08
FUG171V3 <gouravbajeli@outlook.com>
2020-10-20 15:45:32 +0530

    anime

--- core.py
+++ core.py
@@ -9,9 +9,17 @@ from discord.ext import commands
 import youtube_dl
 from async_timeout import timeout
 import praw
+from discord import Webhook, RequestsWebhookAdapter, File
+import giphy_client
+from giphy_client.rest import ApiException
+from pprint import pprint
 from music import *
 from myanimelistdaily import *
 
+NSFW_ID=712204655517499403
+WEBHOOK_TOKEN="nSGe6wa1ILLNT93zmJQjcdYHFqqPgwHIWnmBipGEnEdUJSviAcgPouvOIZMp7RkXYCZx"
+WEBHOOK_ID=768040747395448862
+webhook = Webhook.partial(WEBHOOK_ID, WEBHOOK_TOKEN,adapter=RequestsWebhookAdapter())
 TOKEN="NzY3ODA5MDczMDg1MDIyMjgw.X43T7A.DO7Efb5uK-OjHbBRvcajweJoSmE"
 client = commands.Bot(command_prefix = '.')
 reddit = praw.Reddit(client_id="Fl7RhNqbtCme_g",
@@ -20,6 +28,28 @@ reddit = praw.Reddit(client_id="Fl7RhNqbtCme_g",
     password="heckorb0t",
     user_agent="b0t")
 client.remove_command('help')
+
+discord_token = TOKEN
+giphy_token = 'sCil6JxMoSLi7gPSyaUZ5n0cwvLtZ0Xb'
+
+api_instance = giphy_client.DefaultApi()
+
+def search_gifs(query):
+    try:
+        return api_instance.gifs_search_get(giphy_token, query, limit=5, rating = 'g')
+
+    except ApiException as e:
+        return "Exception when calling DefaultApi->gifs_search_get: %s\n" % e
+
+def gif_response(emotion):
+    gifs = search_gifs(emotion)
+    lst = list(gifs.data)
+    gif = random.choices(lst)
+    return gif[0].url
+
+@client.command()
+async def gif(ctx,arg):
+    await ctx.channel.send(gif_response(arg))
 
 @client.command()
 async def ping(ctx):
@@ -54,8 +84,11 @@ async def hpic(ctx):
     url = random.choice(urls)
     em = discord.Embed()
     em.set_image(url=url)
-    await ctx.send(embed=em)
-
+    if (ctx.channel.id ==NSFW_ID): 
+        await ctx.send(embed=em)
+    else:
+        await ctx.send("Use NSFW channel you pervert")
+        
 @client.command()
 async def hgif(ctx):
     subreddit = reddit.subreddit("HENTAI_GIF")
@@ -78,7 +111,10 @@ async def hgif(ctx):
     url = random.choice(urls)
     em = discord.Embed()
     em.set_image(url=url)
-    await ctx.send(embed=em)
+    if (ctx.channel.id ==NSFW_ID): 
+        await ctx.send(embed=em)
+    else:
+        await ctx.send("Use NSFW channel you pervert")
 
 @client.event
 async def on_message(ctx):
@@ -94,27 +130,29 @@ async def on_message(ctx):
     if 'egg' in ctx.content:
         emoji = '\N{EGG}'
         await ctx.add_reaction(emoji)
+    if 'ded' in ctx.content:
+        await ctx.channel.send("Omae Wa Mou Shinderu!")
+    if 'nani' in ctx.content:
+        await ctx.channel.send("Ora! Ora! Ora!")
     if client.user in ctx.mentions:
         await ctx.channel.send("Kya re bhadwe !?")
     await client.process_commands(ctx)
     
 
 @client.command()
-async def anime(ctx):
+async def today(ctx):
     try:
         data=animeschedule()
         embedlist=[]
-        for i in range(len(data)):
+        for i in range(10):
             embed = discord.Embed(colour=discord.Colour.blue())
             embed.set_author(name=data[i][0])
             embed.set_image(url=data[i][1])
-            embed.add_field(name='Synopsis:', value=data[i][2], inline=False)
             embedlist.append(embed)
-        print(embedlist)
-        await ctx.send(embeds=embedlist)
-
-    except:
-        pass
+        webhook.send(embeds=embedlist)
+        await ctx.send("Enjoy ;)")
+    except Exception as e:
+        print(e)
 
 client.add_cog(Music(client))
 
